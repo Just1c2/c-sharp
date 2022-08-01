27bbb0b64f83347ac3b9e0e9c489be543ed41806
Just1c2 <hieuvtth2110005@fpt.edu.vn>
2022-08-01 08:22:02 +0700

    Add project files.

---
+++ DataType/DataType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataType
+{
+    class Example
+    {
+        static void Main(string[] args)
+        {
+            int intVal;
+            double dbVal;
+            string strVal;
+            intVal = 10;
+            dbVal = 3.142;
+            strVal = "Fpt Aptech";
+            Console.WriteLine("{0} is an integer value", intVal);
+            Console.WriteLine("{0} is an double value", dbVal);
+            Console.WriteLine("{0} is an string", strVal);
+            Console.ReadLine();
+        }
+    }
+}

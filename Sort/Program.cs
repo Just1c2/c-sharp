cfcb786d3b77fb3abc9ecd668b428f4c0f55ab7a
Just1c2 <hieuvtth2110005@fpt.edu.vn>
2022-08-16 10:04:17 +0700

    sort

---
+++ Sort/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Sort
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] arr = new int[5] { 10, 3, 21, 6, 1 };
+            int i, j, temp;
+            for (i = 0; i <= 5; i++)
+            {
+                temp = arr[i];
+                j = i - 1;
+                while (j >= 0 && temp < arr[j])
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+                arr[i + 1] = temp;
+            }
+            Console.WriteLine("\nHien thi mang da duoc sap xep:\n");
+            for(i = 0; i < 5; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            
+        }
+    }
+}

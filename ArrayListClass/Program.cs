dcbd55e8f5db05128854173efc32a96e4e0ed3f4
Just1c2 <hiuv7a@gmail.com>
2022-08-26 08:59:42 +0700

    collections in c#

---
+++ ArrayListClass/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ArrayListClass
+{
+    class Product
+    {
+        string name;
+        double cost;
+        int onhand;
+        public Product(string n, double c, int h)
+        {
+            name = n;
+            cost = c;
+            onhand = h;
+        }
+        public override string ToString()
+        {
+            return String.Format("{0,-10}Cost: {1,6:C} On hand: {2}", name, cost, onhand);
+        }
+    }
+    class Program
+    {
+        public static void Main()
+        {
+            ArrayList inv = new ArrayList();
+
+            //Add elements to the list
+
+            inv.Add(new Product("A", 5.9, 3));
+            inv.Add(new Product("B", 5.29, 2));
+            inv.Add(new Product("C", 8.9, 4));
+            inv.Add(new Product("D", 5.9, 3));
+            inv.Add(new Product("E", 1.8, 8));
+            Console.WriteLine("Product list:");
+            foreach(Product i in inv)
+            {
+                Console.WriteLine(" " + i);
+            }
+        }
+    }
+}
---
+++ ValidPerson/Person.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidPerson
+{
+    class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+
+       public static void Main()
+        {
+            Person pesho = new Person("Pesho", "Peshev", 24);
+            Person fioewr = new Person(string.Empty, "Peshev", 24);
+            Person peshoc = new Person("Pesho", null, 24);
+            Person peshox = new Person("Pesho", "Peshev", -1);
+            Person peshoz= new Person("Pesho", "Peshev", 121);
+
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("value", "First name can't be null or empty");
+                }
+
+                this.firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("value", "Last name can't be null or empty");
+                }
+
+                this.lastName = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+
+            set
+            {
+                if (value < 0 || 120 < value)
+                {
+                    throw new ArgumentOutOfRangeException("value", "Age should be in the range of 0 to 120");
+                }
+
+                this.age = value;
+            }
+        }
+
+        public Person(string firstName, string lastName, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+    }
+}
